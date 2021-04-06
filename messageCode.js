70c349c3fed0b583891a3d27e67ba509225f396e
RudyDaBot <72981271+RudyDaBot@users.noreply.github.com>
2021-04-06 15:38:01 +0530

    Did not add files via upload

---
+++ messageCode.js
@@ -0,0 +1,56 @@
+// Messages for announcements for study group
+
+function sendMsg() {
+    let request = new XMLHttpRequest();
+
+    request.open("POST", "https://discord.com/api/webhooks/828930728808/MR01ansXCnS0p7bPbuwcsgBBC68OVaMkYqCkbAmMF6Y_SnC8vzY7LISKPUkmk5btPFFI")
+    
+    request.setRequestHeader("Content-type", "application/json");
+
+    let params = {
+        Username : "Pop Dog But He Is a Webhook",
+        avatar_url : "",
+        content : "|| @everyone || Announcement Made - Group Study Session Booked in Morning today. Time to be told be Ronak."
+
+    }
+
+    request.send(JSON.stringify(params));
+
+    };
+    function sendMsg2() {
+    let request = new XMLHttpRequest();
+
+    request.open("POST", "https://discord.com/api/webhooks/82893072887499R01ansXCnS0p7bPbuwcgBBC68OVaMkYqCkbAmMF6Y_SnC8vzY7LISKPUkmk5btPFFI")
+    
+    request.setRequestHeader("Content-type", "application/json");
+
+    let params = {
+        Username : "Pop Dog But He Is a Webhook",
+        avatar_url : "",
+        content : "|| @everyone || Announcement Made - Group Study Session Booked in Evening today. Time to be told by Ronak."
+
+    }
+
+    request.send(JSON.stringify(params));
+
+    };
+
+    function sendMsg3() {
+    let request = new XMLHttpRequest();
+
+    request.open("POST", "https://discord.com/api/webhooks/828930728874999808/MR01a0p7bPbuwcsgBBC68OVaMkYqCkbAmMF6Y_SnC8vzY7LISKPUkmk5btPFFI")
+    
+    request.setRequestHeader("Content-type", "application/json");
+
+    let params = {
+        Username : "Pop Dog But He Is a Webhook",
+        avatar_url : "",
+        content : "|| @everyone || Announcement Made - Off topic session being held in the off topic voice channel. If you want to join, you are most welcome to do so."
+
+    }
+
+    request.send(JSON.stringify(params));
+
+    };
+    
+    
